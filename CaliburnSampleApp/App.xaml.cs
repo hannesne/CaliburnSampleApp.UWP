9d87025ef34a6cdea225d8c1df8eb4aba9c5b5fb
hannesne <hannesne@DESKTOP-SI922FS>
2016-05-18 16:11:55 +1200

    Add project files.

---
+++ CaliburnSampleApp/App.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml.Controls;
+using Caliburn.Micro;
+
+namespace CaliburnSampleApp
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App
+    {
+        private WinRTContainer container;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void Configure()
+        {
+            container = new WinRTContainer();
+            container.RegisterWinRTServices();
+
+            //TODO: Register your view models at the container
+            container.PerRequest<MainPageViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            var instance = container.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+            throw new Exception("Could not locate any instances.");
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+
+        protected override void PrepareViewFirst(Frame rootFrame)
+        {
+            container.RegisterNavigationService(rootFrame);
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            if (args.PreviousExecutionState == ApplicationExecutionState.Running)
+                return;
+
+            DisplayRootView<MainPage>();
+        }
+    }
+
+}
---
+++ CaliburnSampleApp/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Input;
+using Caliburn.Micro;
+using GalaSoft.MvvmLight.Command;
+
+namespace CaliburnSampleApp
+{
+    public class MainPageViewModel : PropertyChangedBase
+    {
+        private string messageBlock;
+
+        public MainPageViewModel()
+        {
+            MessageBlock = "Click a button to see a result here.";
+        }
+
+        public ICommand BindingButtonCommand => new RelayCommand(BindingButtonAction);
+
+        private void BindingButtonAction()
+        {
+            MessageBlock = "BindingButton Clicked";
+        }
+
+        public void MessageAttachHandler()
+        {
+            MessageBlock = "MessageAttach Clicked";
+        }
+
+        public void ConventionButton()
+        {
+            MessageBlock = "Convention Button Clicked";
+        }
+
+        public string MessageBlock
+        {
+            get { return messageBlock; }
+            set
+            {
+                if (value == messageBlock) return;
+                messageBlock = value;
+                NotifyOfPropertyChange();
+            }
+        }
+        
+    }
+}
